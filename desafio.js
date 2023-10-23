5b8b23fb931e2764b99e2211e0fbb8a2bee2e50a
ERICK JHONE <erick_jhone@outlook.com>
2023-10-23 20:22:48 -0300

    primeiro commit

---
+++ desafio.js
@@ -0,0 +1,27 @@
+// Variáveis
+const nome = "Seu Herói";
+const xp = 5000;  // Substitua este valor pela quantidade de experiência do herói
+
+// Estrutura de decisão
+let nivel;
+
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 6001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Saída
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
